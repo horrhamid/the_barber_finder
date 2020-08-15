e6ed62846e3da28c4089f538f9be849e6831dc41
hamidreza sadeghi <xhamid0011@gmail.com>
2020-08-16 02:46:33 +0430

    set time

--- the_barber_finder/blog/urls.py
+++ the_barber_finder/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from .views import users_view, set_available_times, reserve_view, choose_subtime
+from .views import users_view, set_available_times, reserve_view, choose_subtime, set_av_time
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', users_view, name='Users'),
     path('auth/', include('authentincation.urls')),
-    path('set_times/', set_available_times, name="set_av_time"),
+    path('set_av_times/', set_available_times, name="set_av_time"),
+    path('set_times/', set_av_time, name="set_time"),
     path('reserve_view/', reserve_view, name='reservation'),
     path('subtime/', choose_subtime, name='subtime'),
 ]
