2fce421c8c9d3f0c2e1ba56e11d5e5ff844494e0
hamidreza sadeghi <xhamid0011@gmail.com>
2020-08-15 14:26:17 +0430

    x

--- the_barber_finder/blog/views.py
+++ the_barber_finder/blog/views.py
@@ -2,14 +2,24 @@
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .models import BarberShop
+from .models import BarberShop, Barber, Person, Customer
 # Create your views here.
 
 
 def users_view(request):
     name = request.user.username
+    print(name)
+    person = Person.objects.filter(user=request.user)
+    print(person)
+    r = Barber.objects.filter(user=person)
+    role = "x"
+    if r[0]:
+        role = "Customer"
+    else:
+        role = "Barber"
     barbershops = BarberShop.objects.all()
-    for x in barbershops:
-        print(x.name)
+
     return render(request, 'dashboard.html', context={'name': name,
-                                                      'shops': barbershops })
+                                                      'shops': barbershops,
+                                                      'role': role, })
+
