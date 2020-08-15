a14d5596fb1d51ac9524f285dbf557ee6b781f4f
masghari78 <mhosseinasghari025@gmail.com>
2020-08-15 14:22:18 +0430

    get_time_table

--- the_barber_finder/blog/views.py
+++ the_barber_finder/blog/views.py
@@ -13,3 +13,14 @@ def users_view(request):
         print(x.name)
     return render(request, 'dashboard.html', context={'name': name,
                                                       'shops': barbershops })
+def create_time_table(request):
+    username = None
+    if request.user.is_authenticated():
+        username = request.user.username
+    barber = request.POST['barber_name']
+    shop = request.POST['shop_name']
+    date = request.POST['date']
+    start_time = request.POST['start_time']
+    end_time = request.POST['end_time']
+    
+
